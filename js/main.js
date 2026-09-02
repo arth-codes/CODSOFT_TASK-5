720f45d26179c3b4cdcfc79b9efa3ccc83beda4e
Arth Jha <arth2006jha@gmail.com>
2026-09-03 00:15:58 +0530

    updated js file to add more functionality

--- js/main.js
+++ js/main.js
@@ -3,6 +3,13 @@
 console.log("Inkora JavaScript loaded successfully.");
 const featuredPostsContainer = document.querySelector("#featuredPosts");
 const latestPostsContainer = document.querySelector("#latestPosts");
+
+const categoryButtons = document.querySelectorAll(".category-btn");
+const loadMoreButton = document.querySelector("#loadMoreBtn");
+
+let selectedCategory = "All";
+let visiblePosts = 3;
+
 function createPostCard(post) {
     return `
         <article class="post-card">
@@ -34,17 +41,57 @@ function createPostCard(post) {
         </article>
     `;
 }
+
+function renderLatestPosts() {
+    let filteredPosts = posts.filter(post => !post.featured);
+
+    if (selectedCategory !== "All") {
+        filteredPosts = filteredPosts.filter(
+            post => post.category === selectedCategory
+        );
+    }
+
+    const postsToDisplay = filteredPosts.slice(0, visiblePosts);
+
+    latestPostsContainer.innerHTML = postsToDisplay
+        .map(post => createPostCard(post))
+        .join("");
+
+    if (visiblePosts >= filteredPosts.length) {
+        loadMoreButton.style.display = "none";
+    } else {
+        loadMoreButton.style.display = "inline-flex";
+    }
+}
 const featuredPosts = posts.filter(post => post.featured);
 
 featuredPostsContainer.innerHTML = featuredPosts
     .map(post => createPostCard(post))
     .join("");
 
-const latestPosts = posts.filter(post => !post.featured);
+renderLatestPosts();
 
-latestPostsContainer.innerHTML = latestPosts
-    .map(post => createPostCard(post))
-    .join("");
+loadMoreButton.addEventListener("click", () => {
+    visiblePosts += 3;
+
+    renderLatestPosts();
+});
+
+categoryButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        selectedCategory = button.dataset.category;
+        visiblePosts = 3;
+
+        categoryButtons.forEach(btn => {
+            btn.classList.remove("active");
+        });
+
+        button.classList.add("active");
+
+        renderLatestPosts();
+    });
+});
 
 
-    
+
+
